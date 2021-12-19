ae1c3e94358454971302ab9956d989f9ab450d08
anamariavargic <anamaria.vargic45@gmail.com>
2021-12-19 21:37:53 +0100

    worked on tilemap

---
+++ core/assets/Maps/watergrass.tsx
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="watergrass" tilewidth="32" tileheight="32" tilecount="18" columns="3">
+ <image source="LPC Base Assets/tiles/watergrass.png" width="96" height="192"/>
+ <wangsets>
+  <wangset name="water edge" type="edge" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="6" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="7" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="8" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="9" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="10" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="11" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="12" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="13" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="14" wangid="0,0,0,0,0,0,0,1"/>
+  </wangset>
+ </wangsets>
+</tileset>
