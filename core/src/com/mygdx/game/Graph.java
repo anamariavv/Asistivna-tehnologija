697ef533fc161cf1e2858f87554f11436d931b7d
anamariavargic <anamaria.vargic45@gmail.com>
2022-01-06 19:21:53 +0100

    Pathifinding

--- core/src/com/mygdx/game/Graph.java
+++ core/src/com/mygdx/game/Graph.java
@@ -1,14 +1,25 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ai.pfa.Connection;
+import com.badlogic.gdx.ai.pfa.indexed.IndexedGraph;
 import com.badlogic.gdx.utils.Array;
 
-public class Graph implements com.badlogic.gdx.ai.pfa.Graph {
+public class Graph implements IndexedGraph {
 
 
     @Override
     public Array<Connection> getConnections(Object fromNode) {
-        //i need to implement this myself
+        //i need to implement this myself, maybe remove this class and implement pfa.graph in graph generator??
         return null;
+    }
+
+    @Override
+    public int getIndex(Object node) {
+        return 0;
+    }
+
+    @Override
+    public int getNodeCount() {
+        return 0;
     }
 }
